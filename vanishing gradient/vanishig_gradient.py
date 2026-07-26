5a3bcccc3a564d5786382350e4b4db2cbda902c6
uavashi <uravashiagrawalofficial@gmail.com>
2026-07-26 23:43:51 +0530

    vanising gradient

---
+++ vanishing gradient/vanishig_gradient.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import keras
+from sklearn.datasets import make_moons
+from sklearn.model_selection import train_test_split
+from keras.layers import Dense
+from keras.models import Sequential
+
+
